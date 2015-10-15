872702a138e84b173e96c428f2fe6cd18d13cebf
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-10-15 10:48:37 -0300

    Ensure that Storms runs at boot time on CentOS.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,10 @@
 unzip -o -q /vagrant/storm-installer-0.9.3-1.el6.x86_64.zip
 rpm -ivh /vagrant/storm-installer-0.9.3-1.el6.x86_64/apache-storm-0.9.3-1.el6.x86_64.rpm  
 rpm -ivh /vagrant/storm-installer-0.9.3-1.el6.x86_64/apache-storm-service-0.9.3-1.el6.x86_64.rpm 
+chkconfig --level 345 storm-nimbus on
+chkconfig --level 345 storm-ui on
+chhconfig --level 345 storm-supervisor on
 service storm-nimbus start
 service storm-ui start
 service storm-supervisor start
+
