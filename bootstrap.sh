351f725e7e2e25bdba5f41e909af5c794aae26a1
JoeBmc <jose_mathew@bmc.com>
2016-04-01 17:34:11 +0530

    Corrected type from chhconfig to chkconfig
    Typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ rpm -ivh /vagrant/storm-installer-0.9.3-1.el6.x86_64/apache-storm-0.9.3-1.el6.x8
 rpm -ivh /vagrant/storm-installer-0.9.3-1.el6.x86_64/apache-storm-service-0.9.3-1.el6.x86_64.rpm 
 chkconfig --level 345 storm-nimbus on
 chkconfig --level 345 storm-ui on
-chhconfig --level 345 storm-supervisor on
+chkconfig --level 345 storm-supervisor on
 service storm-nimbus start
 service storm-ui start
 service storm-supervisor start
