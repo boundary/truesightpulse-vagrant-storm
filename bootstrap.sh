a6d52cdaa1f0623e75ca7164178890eb484ca849
JoeBmc <jose_mathew@bmc.com>
2016-04-01 17:26:48 +0530

    Forced sleep for 20 seconds after unzip.
    Few occasions the unzipped file was not available when using rpm after immediately after the unzip command for centos.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 unzip -o -q /vagrant/storm-installer-0.9.3-1.el6.x86_64.zip
+sleep 20
 rpm -ivh /vagrant/storm-installer-0.9.3-1.el6.x86_64/apache-storm-0.9.3-1.el6.x86_64.rpm  
 rpm -ivh /vagrant/storm-installer-0.9.3-1.el6.x86_64/apache-storm-service-0.9.3-1.el6.x86_64.rpm 
 chkconfig --level 345 storm-nimbus on
